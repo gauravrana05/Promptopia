87f8093ee8a5f386ee005769f24f7024ae73c352
gauravrana05 <ranaofficial05@gmail.com>
2023-08-07 17:13:07 +0530

    tagCursor

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -46,7 +46,7 @@ const PromptCard = ({post , handleTagClick, handleDelete, handleEdit}) => {
         </div>
       </div>
       <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
-      <p className="font-inter text-sm blue_gradient"
+      <p className="font-inter text-sm blue_gradient cursor-pointer"
           onClick={()=>{handleTagClick &&handleTagClick(post.tag)}}
       >#{post.tag} </p>
 

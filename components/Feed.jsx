d0773ec2f445036552bb96a078f2f7b77cf3a353
gauravrana05 <ranaofficial05@gmail.com>
2023-08-07 17:08:41 +0530

    search

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -38,33 +38,33 @@ const Feed = () => {
   }, []);
 
   const filterPrompts = (searchtext) => {
-  //   const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
-  //   return allPosts.filter(
-  //     (item) =>
-  //       regex.test(item.creator.username) ||
-  //       regex.test(item.tag) ||
-  //       regex.test(item.prompt)
-  //   );
+    const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
+    return allPosts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
   };
 
   const handleSearchChange = (e) => {
-    // clearTimeout(searchTimeout);
-    // setSearchText(e.target.value);
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
 
-    // // debounce method
-    // setSearchTimeout(
-    //   setTimeout(() => {
-    //     const searchResult = filterPrompts(e.target.value);
-    //     setSearchedResults(searchResult);
-    //   }, 500)
-    // );
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
   };
 
   const handleTagClick = (tagName) => {
-    // setSearchText(tagName);
+    setSearchText(tagName);
 
-    // const searchResult = filterPrompts(tagName);
-    // setSearchedResults(searchResult);
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
   };
 
   return (
